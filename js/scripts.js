3d7655b1363875e4f3afc4e4238dfba5a9bcb38f
Kirsten <102427504+kir-net@users.noreply.github.com>
2022-04-23 11:05:05 +0200

    task 1.2 requirements and linting added
    scripts.js: deleted previous content, added pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,27 @@ alert( 'Hello world' );
 
 let favoriteFood = 'Spaghetti';
 document.write( favoriteFood );
+
+// array with pokemon objects
+let pokemonList = [
+    {
+        name: "Chamelake",
+        height: 3,
+        types: ['bushes','walls','solid']
+    },
+    {
+        name: "Birdagon",
+        height: 8,
+        types: ['sky','gaseous']
+    },
+    {
+        name: "Kittiger",
+        height: 18,
+        types: ['trees','caves','solid']     
+    },
+    {
+        name: "Turtog",
+        height: 2,
+        types: ['lakes','boats','liquid']
+    }
+]
